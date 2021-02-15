d439100038b99b84f8dc5ea97408ba035670da9b
Henry La Chira <henrylachira1995@gmail.com>
2021-02-14 23:17:22 -0600

    Añadiendo Tarea2 Biblioteca

--- Tarea2-biblioteca/biblioteca.cpp
+++ Tarea2-biblioteca/biblioteca.cpp
@@ -29,24 +29,24 @@ void cargarLibros() {
 	libros[18][0] = "Miscelaneos"; libros[18][1] = "Jefferson Cabrera / "; libros[18][2] = "97 cosas que todo programador deberia saber";
 	libros[19][0] = "Miscelaneos"; libros[19][1] = ""; libros[19][2] = "Docker";
 	libros[20][0] = "Miscelaneos"; libros[20][1] = "Esteban Manchado Velasquez, Joaquin C, Yeray Darias"; libros[20][2] = "El camino a un mejor programador";
-	libros[21][0] = "Miscelaneos"; libros[21][1] = "Introduccion a Docker";
-	libros[22][0] = "Miscelaneos"; libros[22][1] = "Programacion de videojuegos SDL";
-	libros[23][0] = "PHP"; libros[23][1] = "Manual de estudio introductorio al lenguaje PHP procedural";
-	libros[24][0] = "PHP"; libros[24][1] = "PHP y Programacion orientada a objetos";
-	libros[25][0] = "PHP"; libros[25][1] = "POO y MVC en PHP";
-	libros[26][0] = "PHP"; libros[26][1] = "Silex, el manual oficial";
-	libros[27][0] = "PHP"; libros[27][1] = "Symfony 1.4, la guia definitiva";
-	libros[28][0] = "PHP"; libros[28][1] = "Symfony 2.4, el libro oficial";
-	libros[29][0] = "Python"; libros[29][1] = "Aprenda a pensar como un programador (con Python)";
-	libros[30][0] = "Python"; libros[30][1] = "Doma de Serpientes para Ninos: Aprendiendo a Programar con Python";
-	libros[31][0] = "Python"; libros[31][1] = "Inmersion en Python";
-	libros[32][0] = "Python"; libros[32][1] = "Inmersion en Python 3";
-	libros[33][0] = "Python"; libros[33][1] = "Introduccion a la programacion con Python";
-	libros[34][0] = "Python"; libros[34][1] = "Introduccion a Programando con Python";
-	libros[35][0] = "Python"; libros[35][1] = "Python instantaneo (1999)";
-	libros[36][0] = "Python"; libros[36][1] = "Python para ciencia e ingenieria";
-	libros[37][0] = "Python"; libros[37][1] = "Python para principiantes";
-	libros[38][0] = "Python"; libros[38][1] = "Python para todos";
+	libros[21][0] = "Miscelaneos"; libros[21][1] = "José Juan Sánchez Hernández / "; libros[21][2] = "Introduccion a Docker";
+	libros[22][0] = "Miscelaneos"; libros[22][1] = "Alberto García Serrano / "; libros[22][2] = "Programacion de videojuegos SDL";
+	libros[23][0] = "PHP"; libros[23][1] = "Eugenia Bahit / "; libros[23][2] = "Manual de estudio introductorio al lenguaje PHP procedural";
+	libros[24][0] = "PHP"; libros[24][1] = "Diego Lazaro / "; libros[24][2] = "PHP y Programacion orientada a objetos";
+	libros[25][0] = "PHP"; libros[25][1] = "Eugenia Bahit / "; libros[25][2] = "POO y MVC en PHP";
+	libros[26][0] = "PHP"; libros[26][1] = "Fabien Potencier, Igor Wiedler /"; libros[26][2] = "Silex, el manual oficial";
+	libros[27][0] = "PHP"; libros[27][1] = "Fabien Potencier, François Zaninotto"; libros[27][2] = "Symfony 1.4, la guia definitiva";
+	libros[28][0] = "PHP"; libros[28][1] = "Fabien Potencier, Ryan Weave / "; libros[28][2] = "Symfony 2.4, el libro oficial";
+	libros[29][0] = "Python"; libros[29][1] = "Allen Downey,Jeffrey Elkner,Chris Meyers / "; libros[29][2] = "Aprenda a pensar como un programador (con Python)";
+	libros[30][0] = "Python"; libros[30][1] = "Jason R. Briggs / ";libros[30][2] = "Doma de Serpientes para Ninos: Aprendiendo a Programar con Python";
+	libros[31][0] = "Python"; libros[31][1] = "Mark Pilgrim / "; libros[31][2] = "Inmersion en Python";
+	libros[32][0] = "Python"; libros[32][1] = "Mark Pilgrim / ";libros[32][2] = "Inmersion en Python 3";
+	libros[33][0] = "Python"; libros[33][1] = "Andrés Marzal,Isabel Gracia / ";libros[33][2] = "Introduccion a la programacion con Python";
+	libros[34][0] = "Python"; libros[34][1] = "Allen Downey, Jeffrey Elkner, Chris Meyers / ";libros[34][2] = "Introduccion a Programando con Python";
+	libros[35][0] = "Python"; libros[35][1] = "Magnus Lie Hetland / ";libros[35][2] = "Python instantaneo (1999)";
+	libros[36][0] = "Python"; libros[36][1] = "Miguel Angel Suarez Ledo / ";libros[36][2] = "Python para ciencia e ingenieria";
+	libros[37][0] = "Python"; libros[37][1] = "Eugenia Bahit / ";libros[37][2] = "Python para principiantes";
+	libros[38][0] = "Python"; libros[38][1] = "Charles Severance / ";libros[38][2] = "Python para todos";
 }
 
 
